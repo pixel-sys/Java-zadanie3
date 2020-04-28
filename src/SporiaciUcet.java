2c3d7666e9e142172de9421b15d07535080bf1f1
jakub <jakub.skupen44@gmail.com>
2020-04-28 22:54:48 +0200

    test2

--- src/SporiaciUcet.java
+++ src/SporiaciUcet.java
@@ -7,7 +7,7 @@ public class SporiaciUcet extends BeznyUcet
     
     public SporiaciUcet(int id,String typUctu, int cisloUctu, int hotovost,double urok)
     {
-        super(id,typUctu, cisloUctu, hotovost,urok);
+        super(id,typUctu,cisloUctu, hotovost,urok);
         
     }
 }
